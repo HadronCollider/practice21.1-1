60ad93e1a624b1803943eeb95b9f2b00dcf88519
fionderer <85214388+fionderer@users.noreply.github.com>
2021-06-07 09:57:31 +0300

    Added ability to change brightness of photos. Progress isn't saved, when travelling between activities.

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/WorkingScreen.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/WorkingScreen.kt
@@ -1,9 +1,16 @@
 package com.makentoshe.androidgithubcitemplate
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import androidx.appcompat.app.AppCompatActivity
+
 
 class WorkingScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +24,27 @@ class WorkingScreen : AppCompatActivity() {
         next.setOnClickListener{
             val intentTo5 = Intent(this, FinishingScreen::class.java)
             startActivity(intentTo5)
+        }
+        val seek = findViewById<SeekBar>(R.id.seekBar)
+        val photo = findViewById<ImageView>(R.id.image_changing_photo)
+        seek.progress = 128
+        seek.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                photo.colorFilter = changeBrightness(progress)
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+
+
+    }
+    fun changeBrightness(progress: Int): PorterDuffColorFilter {
+        return if (progress >= 100) {
+            val value = (progress - 100) * 255 / 100
+            PorterDuffColorFilter(Color.argb(value, 255, 255, 255), PorterDuff.Mode.SRC_OVER)
+        } else {
+            val value = (100 - progress) * 255 / 100
+            PorterDuffColorFilter(Color.argb(value, 0, 0, 0), PorterDuff.Mode.SRC_ATOP)
         }
     }
 }
