b5115ec6a96ab41456437ad7ebbd4f63f7bb6a59
fionderer <85214388+fionderer@users.noreply.github.com>
2021-06-07 10:48:54 +0300

    Buttons on WorkingScreen allow to choose different things to do with photos. For seekbar only works, when brightness selected.

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/WorkingScreen.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/WorkingScreen.kt
@@ -1,10 +1,10 @@
 package com.makentoshe.androidgithubcitemplate
 
 import android.content.Intent
-import android.graphics.Color
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffColorFilter
+import android.graphics.*
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.SeekBar
@@ -25,17 +25,38 @@ class WorkingScreen : AppCompatActivity() {
             val intentTo5 = Intent(this, FinishingScreen::class.java)
             startActivity(intentTo5)
         }
+        var currentParameter = 'b'
+        val brightness = findViewById<Button>(R.id.brightness_button)
+        brightness.setOnClickListener{
+            Log.d("chosenFeature",currentParameter.toString())
+            currentParameter = 'b'
+        }
+        val contrast = findViewById<Button>(R.id.contrast_button)
+        contrast.setOnClickListener{
+            Log.d("chosenFeature",currentParameter.toString())
+            currentParameter = 'c'
+        }
+        val rotation = findViewById<Button>(R.id.rotation_button)
+        rotation.setOnClickListener{
+            Log.d("chosenFeature",currentParameter.toString())
+            currentParameter = 'r'
+        }
         val seek = findViewById<SeekBar>(R.id.seekBar)
         val photo = findViewById<ImageView>(R.id.image_changing_photo)
         seek.progress = 128
         seek.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                photo.colorFilter = changeBrightness(progress)
+                if (currentParameter == 'b'){
+                    photo.colorFilter = changeBrightness(progress)
+                }
+                if (currentParameter == 'c'){
+                }
+                if (currentParameter == 'r'){
+                }
             }
             override fun onStartTrackingTouch(seekBar: SeekBar) {}
             override fun onStopTrackingTouch(seekBar: SeekBar) {}
         })
-
 
     }
     fun changeBrightness(progress: Int): PorterDuffColorFilter {
