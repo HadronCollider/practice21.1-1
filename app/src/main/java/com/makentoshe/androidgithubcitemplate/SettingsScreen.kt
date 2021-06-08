39377528210d1ff9601c04b1a877cf220217fc2b
fionderer <85214388+fionderer@users.noreply.github.com>
2021-06-08 10:56:50 +0300

    Added ability to change themes in SettingsScreen. Using INSTANCE class to save changes, so they can pe applied on other screens.

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/SettingsScreen.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/SettingsScreen.kt
@@ -1,18 +1,59 @@
 package com.makentoshe.androidgithubcitemplate
 
-import androidx.appcompat.app.AppCompatActivity
+import ThemeHolder
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SettingsScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (ThemeHolder.INSTANCE.chosenTheme == 'w'){
+            setTheme(R.style.AppTheme)
+        }
+        if (ThemeHolder.INSTANCE.chosenTheme == 'd'){
+            setTheme(R.style.DarkTheme)
+        }
+        if (ThemeHolder.INSTANCE.chosenTheme == 'b'){
+            setTheme(R.style.BeachTheme)
+        }
+        if (ThemeHolder.INSTANCE.chosenTheme == 'g'){
+            setTheme(R.style.GreenTheme)
+        }
         setContentView(R.layout.activity_settings_screen)
         val prev = findViewById<Button>(R.id.button41)
         prev.setOnClickListener{
+            ThemeHolder.INSTANCE.changeTheme(ThemeHolder.INSTANCE.realTheme)
             finish()
         }
         val save = findViewById<Button>(R.id.button42)
-
+        val white = findViewById<Button>(R.id.white_button)
+        val black = findViewById<Button>(R.id.black_button)
+        val beach = findViewById<Button>(R.id.beach_button)
+        val green = findViewById<Button>(R.id.green_button)
+        white.setOnClickListener{
+            ThemeHolder.INSTANCE.changeTheme('w')
+            recreate()
+        }
+        black.setOnClickListener{
+            ThemeHolder.INSTANCE.changeTheme('d')
+            recreate()
+        }
+        beach.setOnClickListener{
+            ThemeHolder.INSTANCE.changeTheme('b')
+            recreate()
+        }
+        green.setOnClickListener{
+            ThemeHolder.INSTANCE.changeTheme('g')
+            recreate()
+        }
+        save.setOnClickListener{
+            ThemeHolder.INSTANCE.changeRealTheme(ThemeHolder.INSTANCE.chosenTheme)
+            ThemeHolder.INSTANCE.mainNeedsToBeChanged = true
+            finish()
+        }
     }
 }
