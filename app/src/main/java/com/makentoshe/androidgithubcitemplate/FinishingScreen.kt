d21aa80305b1ea5057e01f1c85d8a41b994158ce
princebelkovetz <belkovetzhse@gmail.com>
2021-06-11 01:00:45 +0300

    merged photo saving + master branch

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/FinishingScreen.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/FinishingScreen.kt
@@ -1,16 +1,20 @@
 package com.makentoshe.androidgithubcitemplate
 
 import ThemeHolder
+import android.content.ContentValues
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileNotFoundException
+import java.io.*
 
 
 class FinishingScreen : AppCompatActivity() {
@@ -38,19 +42,60 @@ class FinishingScreen : AppCompatActivity() {
             finish()
         }
         val save = findViewById<Button>(R.id.button52)
+        val img = findViewById<ImageView>(R.id.resultImage)
+        save.setOnClickListener {
+            img.setDrawingCacheEnabled(true)
+            img.buildDrawingCache()
+            val bm: Bitmap = img.getDrawingCache()
+            val baos = ByteArrayOutputStream()
+            bm.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+            saveBitmapAsImageToDevice(bm)
+        }
     }
     private fun loadImageFromStorage(path: String) {
         try {
             val f = File(path, "profile.jpg")
-            Log.d("debug", f.absolutePath);
             val b = BitmapFactory.decodeStream(FileInputStream(f))
-            Log.d("debug", "b")
             val img = findViewById<ImageView>(R.id.resultImage)
-            Log.d("debug", "img")
-            Log.d("debug", b.toString())
             img.setImageBitmap(b)
         } catch (e: FileNotFoundException) {
             e.printStackTrace()
+        }
+    }
+    private fun saveBitmapAsImageToDevice(bitmap: Bitmap?) {
+        // Add a specific media item.
+        val resolver = this.contentResolver
+
+        val imageStorageAddress = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+        } else {
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+
+        val imageDetails = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, "my_app_${System.currentTimeMillis()}.jpg")
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            put(MediaStore.MediaColumns.DATE_ADDED, System.currentTimeMillis())
+        }
+
+        try {
+            // Save the image.
+            val contentUri: Uri? = resolver.insert(imageStorageAddress, imageDetails)
+            contentUri?.let { uri ->
+                // Don't leave an orphan entry in the MediaStore
+                if (bitmap == null) resolver.delete(contentUri, null, null)
+                val outputStream: OutputStream? = resolver.openOutputStream(uri)
+                outputStream?.let { outStream ->
+                    val isBitmapCompressed =
+                        bitmap?.compress(Bitmap.CompressFormat.JPEG, 95, outStream)
+                    if (isBitmapCompressed == true) {
+                        outStream.flush()
+                        outStream.close()
+                    }
+                } ?: throw IOException("Failed to get output stream.")
+            } ?: throw IOException("Failed to create new MediaStore record.")
+        } catch (e: IOException) {
+            throw e
         }
     }
 }
