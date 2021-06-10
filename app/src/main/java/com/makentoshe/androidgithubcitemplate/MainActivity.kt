9d9cf4532da3a5ff3b08c2cd9b9fa2bae3566bac
A8stern <kovgleb04@mail.ru>
2021-06-10 21:42:39 +0300

    Сохранение фотографий, но в чем-то есть ошибка

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -1,29 +1,102 @@
 package com.makentoshe.androidgithubcitemplate
 
+import android.content.ContentValues
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     val RQ = 101
+    val RQ1 = 228
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        val flower16 = findViewById<ImageView>(R.id.imageView)
+        val buttonsave = findViewById<Button>(R.id.button2)
+        buttonsave.setOnClickListener{
+            saveBitmapAsImageToDevice(getBitmapFromView(flower16))
+        }
+
+
         buttonTaps()
     }
+
+
+    private fun saveBitmapAsImageToDevice(bitmap: Bitmap?) {
+        // Add a specific media item.
+        val resolver = this.contentResolver
+
+        val imageStorageAddress = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+        } else {
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+
+        val imageDetails = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, "my_app_${System.currentTimeMillis()}.jpg")
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            put(MediaStore.MediaColumns.DATE_ADDED, System.currentTimeMillis())
+        }
+
+        try {
+            // Save the image.
+            val contentUri: Uri? = resolver.insert(imageStorageAddress, imageDetails)
+            contentUri?.let { uri ->
+                // Don't leave an orphan entry in the MediaStore
+                if (bitmap == null) resolver.delete(contentUri, null, null)
+                val outputStream: OutputStream? = resolver.openOutputStream(uri)
+                outputStream?.let { outStream ->
+                    val isBitmapCompressed =
+                        bitmap?.compress(Bitmap.CompressFormat.JPEG, 95, outStream)
+                    if (isBitmapCompressed == true) {
+                        outStream.flush()
+                        outStream.close()
+                    }
+                } ?: throw IOException("Failed to get output stream.")
+            } ?: throw IOException("Failed to create new MediaStore record.")
+        } catch (e: IOException) {
+            throw e
+        }
+    }
+    fun getBitmapFromView(view: View): Bitmap {
+        return Bitmap.createBitmap(view.width, view.height,Bitmap.Config.ARGB_8888).apply {
+            Canvas(this).apply {
+                view.draw(this)
+            }
+        }
+    }
+
+
 
     private fun buttonTaps(){
         button.setOnClickListener {
             checkforPermissions(android.Manifest.permission.READ_EXTERNAL_STORAGE, "read media files", RQ)
+            checkforPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, "read media files", RQ)
         }
 
     }
@@ -52,7 +125,7 @@ class MainActivity : AppCompatActivity() {
 
         when (requestCode){
             RQ -> innerCheck("read media files")
-
+            RQ1 -> innerCheck("write media files")
         }
     }
 
